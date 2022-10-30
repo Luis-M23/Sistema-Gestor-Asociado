fb4c8a36c621a9ea7ea249ebc38f0d98c25aa793
Luis-M23 <90297281+Luis-M23@users.noreply.github.com>
2022-10-30 03:08:33 -0600

    Iniciando Proyecto

---
+++ Sistema_Gestor_Asociado/src/main/java/DT/SGA/SistemaGestorAsociadoApplication.java
@@ -0,0 +1,13 @@
+package DT.SGA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestorAsociadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGestorAsociadoApplication.class, args);
+	}
+
+}
